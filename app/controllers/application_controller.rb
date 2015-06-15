9b616f5cbbe17d0bf8e3ebf08e211affdc86ebca
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-15 12:47:01 -0400

    Fix Rails From Blocking Login From Console

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::API
 
   before_action :authenticate_user_from_token!
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
   respond_to :json
 
   ## 
